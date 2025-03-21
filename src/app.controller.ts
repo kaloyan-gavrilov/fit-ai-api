852904632cab507dc7c5e67ead6ab2d07741997c
Kaloyan Gavrilov <75575061+kaloyan-gavrilov@users.noreply.github.com>
2025-03-21 02:42:21 +0200

    feat: add generate-plan endpoint and set HTTP status code for process-query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ProcessQueryRequestBodyDto } from './dtos/process-query-request-body.dto';
+import { GeneratePlanRequestBodyDto } from './dtos/generate-plan-request-body.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post("/process-query")
+  @HttpCode(200)
   async postProcessQuery(@Body() body: ProcessQueryRequestBodyDto) {
     console.log(body);
     return this.appService.processQuery(body.nameUser, body.userInfo, body.query);
+  }
+
+  @Post('generate-plan')
+  async generatePlan(@Body() body: GeneratePlanRequestBodyDto) {
+    return this.appService.generatePlan(body.variant, body.userInfo, body.pastExperiences);
   }
 }
